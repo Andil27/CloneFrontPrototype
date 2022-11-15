2e10131bf1ce0c027e0b9ae805165ff64da7c23a
andil <andildine2@gmail.com>
2022-11-15 10:54:24 +0100

    first commit

---
+++ src/components/PagePost.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="getPosts">Load Posts</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "PagePost",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    getPosts() {
+      console.log("inna as-sidq yahdi ila abirr");
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/page/homePage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="homePage">
+    <RestaurantRow />
+    <PagePost />
+  </div>
+</template>
+
+<script>
+//IMPORT
+import BDD from "../BDD";
+//COMPONENTS
+import RestaurantRow from "../components/RestaurantRow.vue";
+import PagePost from "../components/PagePost.vue";
+export default {
+  name: "HomePage",
+  components: {
+    RestaurantRow,
+    PagePost,
+  },
+  setup() {
+    console.log(BDD);
+    class Restaurant {
+      constructor(name, note, image, drive_time) {
+        this.name = name;
+        this.note = note;
+        this.image = image;
+        this.drive_time = drive_time;
+      }
+    }
+
+    let data_restaurant = [];
+
+    const makeDataRestaurant = () => {
+      let three_restaurant = [];
+
+      for (const restaurant of BDD) {
+        const new_restaurant = new Restaurant(
+          restaurant.name,
+          restaurant.note,
+          restaurant.image,
+          restaurant.drive_time
+        );
+
+        three_restaurant.push(new_restaurant);
+
+        if (three_restaurant.length === 3) {
+          data_restaurant.push(three_restaurant);
+          three_restaurant = [];
+        }
+      }
+
+      console.log("data_restaurant :", data_restaurant);
+    };
+    makeDataRestaurant();
+  },
+};
+</script>
+
+<style></style>
